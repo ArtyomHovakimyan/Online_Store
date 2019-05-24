7364140bfe785b47e5510af84bd268f312ab545e
User <artyom.hovakimyan01@gmail.com>
2019-05-25 00:42:21 +0400

    add models

---
+++ Online_Mobile_Store/Models/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_Mobile_Store.Models
+{
+    public class Phone
+    {
+        public int PhoneId { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsPreferredDrink { get; set; }
+        public bool InStock { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
